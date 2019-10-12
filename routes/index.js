2ded918d654846c6f77ba51994861ffdae477e80
Wyatt Phillips <datscigye@gmail.com>
2019-10-11 21:50:32 -0400

    Make stream id a var

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ module.exports = function () {
     var route = require('express').Router();
 
     route.get('/', (req, res) => {
-        res.render('feed', {layout: 'feed'});
+        // steam_id is random example.
+        res.render('feed', {layout: 'feed', stream_id: '13b5xd'});
     });
 
     return route;
