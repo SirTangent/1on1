3da3808422cf403a2798652cc76859601d251de5
Wyatt Phillips <datscigye@gmail.com>
2019-10-11 20:02:55 -0400

    Init npm & express app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+var app = express();
+
+// Establish Connection
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server listening @ ${port}`);
+});
