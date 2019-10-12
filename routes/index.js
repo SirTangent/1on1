33945cf987c3938e2f0a12fa98d11731d4c7bff9
Wyatt Phillips <datscigye@gmail.com>
2019-10-12 02:16:31 -0400

    Populate index and about page

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 module.exports = function () {
     var route = require('express').Router();
 
+    // Index Page Route
+    route.get('/', (req, res) => {
+        // steam_id is random example.
+        res.render('index', {layout: 'gradient'});
+    });
+
+    // Stream Page Route
     route.get('/:id', (req, res) => {
         // steam_id is random example.
         res.render('stream', {layout: 'gradient', stream_id: req.params.id});
     });
 
-    route.get('/', (req, res) => {
-        // steam_id is random example.
-        res.render('index', {layout: 'gradient'});
+    // About Page Route
+    route.post('/f/create', (req, res) => {
+        //TODO: Add page generation!
+    });
+
+    // About Page Route
+    route.get('/f/about', (req, res) => {
+       res.render('about',{layout: 'gradient'});
     });
 
     return route;
