fda12ec4ce238a51b467e060cb206bc9809338d3
Wyatt Phillips <datscigye@gmail.com>
2019-10-11 21:43:44 -0400

    Add feed page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var route = require('express').Router();
 
     route.get('/', (req, res) => {
-        res.render('index', {layout: 'gradient'});
+        res.render('feed', {layout: 'feed'});
     });
 
     return route;
