afd21122709868232aa4eefcab7a4cd5c807a636
Wyatt Phillips <datscigye@gmail.com>
2019-10-11 20:42:12 -0400

    Add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 var app = express();
+
+// Use handlebars engine
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 // Get routes
 var indexRoutes = require('./routes/index');
